3d088cdcc86dba915b321181cc5d60599bd340e1
Francois Laupretre <francois.laupretre@ontexglobal.com>
2021-02-17 12:13:43 +0000

    Allow creation of delegation NS records in parent domain

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module "zones" {
+module "zone" {
   source = "../../modules/zones"
 
   zones = {
@@ -13,8 +13,19 @@ module "zones" {
       }
     }
 
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+
+module "subzones" {
+  source = "../../modules/zones"
+
+  zones = {
     "app.terraform-aws-modules-example.com" = {
       comment = "app.terraform-aws-modules-example.com"
+      parent_id = module.zone.this_route53_zone_zone_id
       tags = {
         Name = "app.terraform-aws-modules-example.com"
       }
@@ -22,6 +33,7 @@ module "zones" {
 
     "private-vpc.terraform-aws-modules-example.com" = {
       comment = "private-vpc.terraform-aws-modules-example.com"
+      parent_id = module.zone.this_route53_zone_zone_id
       vpc = [
         {
           vpc_id = module.vpc.vpc_id
@@ -44,8 +56,8 @@ module "zones" {
 module "records" {
   source = "../../modules/records"
 
-  zone_name = keys(module.zones.this_route53_zone_zone_id)[0]
-  #  zone_id = module.zones.this_route53_zone_zone_id["terraform-aws-modules-example.com"]
+  zone_name = keys(module.zone.this_route53_zone_zone_id)[0]
+  #  zone_id = module.zone.this_route53_zone_zone_id["terraform-aws-modules-example.com"]
 
   records = [
     {
@@ -94,7 +106,7 @@ module "records" {
     }
   ]
 
-  depends_on = [module.zones]
+  depends_on = [module.zone]
 }
 
 module "disabled_records" {
